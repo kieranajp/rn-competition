e9c89d85c32fb58ffbd91af6562fda737ccebc92
Kieran <me@kieranajp.co.uk>
2018-06-21 17:29:00 +0200

    Design stuffs

--- App.js
+++ App.js
@@ -15,27 +15,25 @@ export default class App extends React.Component {
 
         return ( 
             <View style={styles.container}>
-                <View>
-                    {Array.from({ length: App.numPlayers }, (v, i) =>
-                        <Subscribe
-                            key={i}
-                            to={<CounterModel
-                                collection={`player_${i + 1}_${week}`}
-                                week={week}
-                                day={day} />
-                            }
-                        >
-                            {counterModel =>
-                                <Counter
-                                    title={`Player ${i + 1}`}
-                                    isLoading={counterModel.isLoading}
-                                    dailyScore={counterModel.dailyScore}
-                                    weeklyScore={counterModel.weeklyScore}
-                                    increment={counterModel.increment} />
-                            }
-                        </Subscribe>
-                    )}
-                </View>
+                {Array.from({ length: App.numPlayers }, (v, i) =>
+                    <Subscribe
+                        key={i}
+                        to={<CounterModel
+                            collection={`player_${i + 1}_${week}`}
+                            week={week}
+                            day={day} />
+                        }
+                    >
+                        {counterModel =>
+                            <Counter
+                                index={i + 1}
+                                isLoading={counterModel.isLoading}
+                                dailyScore={counterModel.dailyScore}
+                                weeklyScore={counterModel.weeklyScore}
+                                increment={counterModel.increment} />
+                        }
+                    </Subscribe>
+                )}
             </View>
         )
     }
@@ -44,8 +42,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
 })
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,22 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Text, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 const Counter = (props) => {
     return (
-        <View>
-            <Text>This week: {props.weeklyScore}</Text>
-            <Text>Today: {props.dailyScore}</Text>
-            <Button title="Clickums" disabled={props.isLoading} onPress={props.increment} />
-        </View>
+        <TouchableOpacity
+            style={[ styles.container, styles[`variant_${props.index}`] ]}
+            disabled={props.isLoading}
+            onPress={props.increment}>
+            <View>
+                <Text style={styles.title}>Player {props.index}</Text>
+                {props.isLoading ?
+                    <View>
+                        <Text style={styles.score}>Loading</Text>
+                        <ActivityIndicator size="small" color="black" />
+                    </View>:
+                    <View>
+                        <Text style={styles.score}>Today: {props.dailyScore}</Text>
+                        <Text style={styles.score}>This week: {props.weeklyScore}</Text>
+                    </View>
+                }
+            </View>
+        </TouchableOpacity>
     )
 }
 
 Counter.propTypes = {
-    title: PropTypes.string,
+    index: PropTypes.number,
     dailyScore: PropTypes.number,
     weeklyScore: PropTypes.number,
     increment: PropTypes.func,
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    variant_1: {
+        backgroundColor: 'powderblue',
+    },
+    variant_2: {
+        backgroundColor: 'skyblue'
+    },
+    title: {
+        fontFamily: 'sans-serif-condensed',
+        textAlign: 'center',
+        fontSize: 62,
+        color: 'white',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    score: {
+        fontFamily: 'sans-serif-condensed',
+        fontSize: 22,
+        textAlign: 'center'
+    }
+})
 
 export default Counter
